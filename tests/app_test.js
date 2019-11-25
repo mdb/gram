946d92c496aa89411151c1a704c082eb3d433840
Mike Ball <mikedball@gmail.com>
2019-11-25 15:05:39 -0500

    rename index.js as app.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,7 +1,7 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const nock = require('nock')
-const app = require('../index')
+const app = require('../app')
 const expect = chai.expect
 
 chai.use(chaiHttp)
